c083b2fd0bdc6302f89a84c0f30c75cb0b079853
INODGEEKZ <donisanjaya9798@gmail.com>
2022-12-05 15:28:26 +0700

    pterodactyl

---
+++ install.sh
@@ -0,0 +1,115 @@
+#!/bin/bash
+
+set -e
+
+#############################################################################
+#                                                                           #
+# Project 'pterodactyl-installer'                                           #
+#                                                                           #
+# Copyright (C) 2018 - 2022, Vilhelm Prytz, <vilhelm@prytznet.se>           #
+#                                                                           #
+#   This program is free software: you can redistribute it and/or modify    #
+#   it under the terms of the GNU General Public License as published by    #
+#   the Free Software Foundation, either version 3 of the License, or       #
+#   (at your option) any later version.                                     #
+#                                                                           #
+#   This program is distributed in the hope that it will be useful,         #
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of          #
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           #
+#   GNU General Public License for more details.                            #
+#                                                                           #
+#   You should have received a copy of the GNU General Public License       #
+#   along with this program.  If not, see <https://www.gnu.org/licenses/>.  #
+#                                                                           #
+# https://github.com/vilhelmprytz/pterodactyl-installer/blob/master/LICENSE #
+#                                                                           #
+# This script is not associated with the official Pterodactyl Project.      #
+# https://github.com/vilhelmprytz/pterodactyl-installer                     #
+#                                                                           #
+#############################################################################
+
+export GITHUB_SOURCE="v0.12.0"
+export SCRIPT_RELEASE="v0.12.0"
+export GITHUB_BASE_URL="https://raw.githubusercontent.com/vilhelmprytz/pterodactyl-installer"
+
+LOG_PATH="/var/log/pterodactyl-installer.log"
+
+# check for curl
+if ! [ -x "$(command -v curl)" ]; then
+  echo "* curl is required in order for this script to work."
+  echo "* install using apt (Debian and derivatives) or yum/dnf (CentOS)"
+  exit 1
+fi
+
+if [ ! -f "/tmp/lib.sh" ]; then
+  # Until first official major-refactor release this needs to always be pulled from master
+  curl -sSL -o /tmp/lib.sh "$GITHUB_BASE_URL"/"$GITHUB_SOURCE"/lib/lib.sh
+fi
+
+source /tmp/lib.sh
+
+execute() {
+  echo -e "\n\n* pterodactyl-installer $(date) \n\n" >>$LOG_PATH
+
+  [[ "$1" == *"canary"* ]] && export GITHUB_SOURCE="master" && export SCRIPT_RELEASE="canary"
+  update_lib_source
+  run_ui "${1//_canary/}" |& tee -a $LOG_PATH
+
+  if [[ -n $2 ]]; then
+    echo -e -n "* Installation of $1 completed. Do you want to proceed to $2 installation? (y/N): "
+    read -r CONFIRM
+    if [[ "$CONFIRM" =~ [Yy] ]]; then
+      execute "$2"
+    else
+      error "Installation of $2 aborted."
+      exit 1
+    fi
+  fi
+}
+
+welcome
+
+done=false
+while [ "$done" == false ]; do
+  options=(
+    "Install the panel"
+    "Install Wings"
+    "Install both [0] and [1] on the same machine (wings script runs after panel)"
+    # "Uninstall panel or wings\n"
+
+    "Install panel with canary version of the script (the versions that lives in master, may be broken!)"
+    "Install Wings with canary version of the script (the versions that lives in master, may be broken!)"
+    "Install both [3] and [4] on the same machine (wings script runs after panel)"
+    "Uninstall panel or wings with canary version of the script (the versions that lives in master, may be broken!)"
+  )
+
+  actions=(
+    "panel"
+    "wings"
+    "panel;wings"
+    # "uninstall"
+
+    "panel_canary"
+    "wings_canary"
+    "panel_canary;wings_canary"
+    "uninstall_canary"
+  )
+
+  output "What would you like to do?"
+
+  for i in "${!options[@]}"; do
+    output "[$i] ${options[$i]}"
+  done
+
+  echo -n "* Input 0-$((${#actions[@]} - 1)): "
+  read -r action
+
+  [ -z "$action" ] && error "Input is required" && continue
+
+  valid_input=("$(for ((i = 0; i <= ${#actions[@]} - 1; i += 1)); do echo "${i}"; done)")
+  [[ ! " ${valid_input[*]} " =~ ${action} ]] && error "Invalid option"
+  [[ " ${valid_input[*]} " =~ ${action} ]] && done=true && IFS=";" read -r i1 i2 <<<"${actions[$action]}" && execute "$i1" "$i2"
+done
+
+# Remove lib.sh, so next time the script is run the, newest version is downloaded.
+rm -rf /tmp/lib.sh
---
+++ installers/uninstall.sh
@@ -0,0 +1,168 @@
+#!/bin/bash
+
+set -e
+
+#############################################################################
+#                                                                           #
+# Project 'pterodactyl-installer'                                           #
+#                                                                           #
+# Copyright (C) 2018 - 2022, Vilhelm Prytz, <vilhelm@prytznet.se>           #
+#                                                                           #
+#   This program is free software: you can redistribute it and/or modify    #
+#   it under the terms of the GNU General Public License as published by    #
+#   the Free Software Foundation, either version 3 of the License, or       #
+#   (at your option) any later version.                                     #
+#                                                                           #
+#   This program is distributed in the hope that it will be useful,         #
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of          #
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           #
+#   GNU General Public License for more details.                            #
+#                                                                           #
+#   You should have received a copy of the GNU General Public License       #
+#   along with this program.  If not, see <https://www.gnu.org/licenses/>.  #
+#                                                                           #
+# https://github.com/vilhelmprytz/pterodactyl-installer/blob/master/LICENSE #
+#                                                                           #
+# This script is not associated with the official Pterodactyl Project.      #
+# https://github.com/vilhelmprytz/pterodactyl-installer                     #
+#                                                                           #
+#############################################################################
+
+# Check if script is loaded, load if not or fail otherwise.
+fn_exists() { declare -F "$1" >/dev/null; }
+if ! fn_exists lib_loaded; then
+  # shellcheck source=lib/lib.sh
+  source /tmp/lib.sh || source <(curl -sSL "$GITHUB_BASE_URL/$GITHUB_SOURCE"/lib/lib.sh)
+  ! fn_exists lib_loaded && echo "* ERROR: Could not load lib script" && exit 1
+fi
+
+# ------------------ Variables ----------------- #
+
+RM_PANEL="${RM_PANEL:-true}"
+RM_WINGS="${RM_WINGS:-true}"
+
+# ---------- Uninstallation functions ---------- #
+
+rm_panel_files() {
+  output "Removing panel files..."
+  rm -rf /var/www/pterodactyl /usr/local/bin/composer
+  [ "$OS" != "centos" ] && unlink /etc/nginx/sites-enabled/pterodactyl.conf
+  [ "$OS" != "centos" ] && rm -f /etc/nginx/sites-available/pterodactyl.conf
+  [ "$OS" != "centos" ] && ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
+  [ "$OS" == "centos" ] && rm -f /etc/nginx/conf.d/pterodactyl.conf
+  systemctl restart nginx
+  success "Removed panel files."
+}
+
+rm_docker_containers() {
+  output "Removing docker containers and images..."
+
+  docker system prune -a -f
+
+  success "Removed docker containers and images."
+}
+
+rm_wings_files() {
+  output "Removing wings files..."
+
+  # stop and remove wings service
+  systemctl disable --now wings
+  rm -rf /etc/systemd/system/wings.service
+
+  rm -rf /etc/pterodactyl /usr/local/bin/wings /var/lib/pterodactyl
+  success "Removed wings files."
+}
+
+rm_services() {
+  output "Removing services..."
+  systemctl disable --now pteroq
+  rm -rf /etc/systemd/system/pteroq.service
+  case "$OS" in
+  debian | ubuntu)
+    systemctl disable --now redis-server
+    ;;
+  centos)
+    systemctl disable --now redis
+    systemctl disable --now php-fpm
+    rm -rf /etc/php-fpm.d/www-pterodactyl.conf
+    ;;
+  esac
+  success "Removed services."
+}
+
+rm_cron() {
+  output "Removing cron jobs..."
+  crontab -l | grep -vF "* * * * * php /var/www/pterodactyl/artisan schedule:run >> /dev/null 2>&1" | crontab -
+  success "Removed cron jobs."
+}
+
+rm_database() {
+  output "Removing database..."
+  valid_db=$(mysql -u root -e "SELECT schema_name FROM information_schema.schemata;" | grep -v -E -- 'schema_name|information_schema|performance_schema|mysql')
+  warning "Be careful! This database will be deleted!"
+  if [[ "$valid_db" == *"panel"* ]]; then
+    echo -n "* Database called panel has been detected. Is it the pterodactyl database? (y/N): "
+    read -r is_panel
+    if [[ "$is_panel" =~ [Yy] ]]; then
+      DATABASE=panel
+    else
+      print_list "$valid_db"
+    fi
+  else
+    print_list "$valid_db"
+  fi
+  while [ -z "$DATABASE" ] || [[ $valid_db != *"$database_input"* ]]; do
+    echo -n "* Choose the panel database (to skip don't input anything): "
+    read -r database_input
+    if [[ -n "$database_input" ]]; then
+      DATABASE="$database_input"
+    else
+      break
+    fi
+  done
+  [[ -n "$DATABASE" ]] && mysql -u root -e "DROP DATABASE $DATABASE;"
+  # Exclude usernames User and root (Hope no one uses username User)
+  output "Removing database user..."
+  valid_users=$(mysql -u root -e "SELECT user FROM mysql.user;" | grep -v -E -- 'user|root')
+  warning "Be careful! This user will be deleted!"
+  if [[ "$valid_users" == *"pterodactyl"* ]]; then
+    echo -n "* User called pterodactyl has been detected. Is it the pterodactyl user? (y/N): "
+    read -r is_user
+    if [[ "$is_user" =~ [Yy] ]]; then
+      DB_USER=pterodactyl
+    else
+      print_list "$valid_users"
+    fi
+  else
+    print_list "$valid_users"
+  fi
+  while [ -z "$DB_USER" ] || [[ $valid_users != *"$user_input"* ]]; do
+    echo -n "* Choose the panel user (to skip don't input anything): "
+    read -r user_input
+    if [[ -n "$user_input" ]]; then
+      DB_USER=$user_input
+    else
+      break
+    fi
+  done
+  [[ -n "$DB_USER" ]] && mysql -u root -e "DROP USER $DB_USER@'127.0.0.1';"
+  mysql -u root -e "FLUSH PRIVILEGES;"
+  success "Removed database and database user."
+}
+
+# --------------- Main functions --------------- #
+
+perform_uninstall() {
+  [ "$RM_PANEL" == true ] && rm_panel_files
+  [ "$RM_PANEL" == true ] && rm_cron
+  [ "$RM_PANEL" == true ] && rm_database
+  [ "$RM_PANEL" == true ] && rm_services
+  [ "$RM_WINGS" == true ] && rm_docker_containers
+  [ "$RM_WINGS" == true ] && rm_wings_files
+
+  return 0
+}
+
+# ------------------ Uninstall ----------------- #
+
+perform_uninstall
